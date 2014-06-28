f86829de0ebc0d2ca734e9702d0820a5d2f48d96
Sammons <bsammnz@gmail.com>
2014-06-28 10:04:26 -0500

    i like this approach much better, reworked tokenizer

--- stream.js
+++ stream.js
@@ -3,110 +3,55 @@ var fs = require('fs');
 var args = process.argv.slice(2);
 if (args.length == 0) return console.log("not enough args");
 
-var firstFile = fs.readFileSync(args[0],'utf8');
-
-
-var vocabulary = {
-	"WORD"  : /[%&\*^]+/,
-	"sVAR"   : /[A-Z\*]+/,
-	"lVAR"  : /[a-z]+/,
-	"TWOWORDS" : /[a-z]+\s[a-z]+/,
-	"TRASH" : /\s+/g// REQUIRED not use *
+tokens = {
+	IF: /^if/,
+	LCURL: /^\{/,
+	RCURL: /^\}/,
+	STRING: /^\".*?\"|^\'.*?\'/,
+	MULTILINE_COMMENT: /^\/\*(.|\s)*?\*\//gim,
+	COMMENT: /^\/\/.*?$/,
+	NUMBER: /^[0-9\.]+/,
+	BITSHIFT: /^\>\>|^\<\</,
+	STMT_END: /^\n|^\;/,
+	IGNORE: /^\s/
 }
 
+non_terminals = {
+
+}
+
+var plaintext = fs.readFileSync(args[0])+'';
+var backup_plaintext = plaintext;
+var tokens_matched = []
 var stack = [];
-var grammar = {
-	"exp" : {
-		match : [[ "WORD" ], ["exp", "WORD"]],
-		actions : [ 
-			function(terminalTokens) {
 
-			} 
-		]
-	} 
-}
+do {
+	var next = next_token(plaintext);
+	if (next.match != '') tokens_matched.push(next)
+	plaintext = plaintext.replace(next.match,'');
+	plaintext = plaintext.replace(tokens.IGNORE, '')
+} while (next.match != '');
 
-function getRegexpStr(reg) {
-	return reg.toString().replace(/^\/|\/.*$/g,'');
-}
-function trashTrim(str) {
-	var rtrash = getRegexpStr(vocabulary["TRASH"]);
-	var trtrm = new RegExp(
-		"^"+rtrash+"|"+rtrash+"$"
-		,'g');
-	return str.replace(trtrm, '');
-}
-function trashTrimFront(str) {
-	var rtrash = getRegexpStr(vocabulary["TRASH"]);
-	var trtrm = new RegExp(
-		"^"+rtrash
-		,'g');
-	return str.replace(trtrm, '');
+if (plaintext != '') {
+	console.log("UNRECOGNIZED SYMBOL FOUND: "+plaintext);
+	return;
 }
 
-var tokenStack = [];
-var trash = getRegexpStr(vocabulary["TRASH"]);
-function processFile(raw) {
-	while (raw) {
-		raw = trashTrimFront(raw);
-		var match = findBestMatch(raw);
-		if (match.match) {
-			tokenStack.push({word: match.word, val: match.match});
-			raw = raw.replace(
-				new RegExp("^"+getRegexpStr(vocabulary[match.word])),'')
-		} else {
-			if (trashTrim(raw).length != 0)
-				console.log("INVALID TOKENS:"+raw)
-			break;
-		}
+console.log(tokens_matched)
+
+function next_token(text, ignore) {
+	var matches = [];
+	for (var i in tokens) {
+		var tok_matches = text.match(tokens[i]);
+		for (var j in tok_matches) (function(){tok_matches[j] = {tok: i, match: tok_matches[j]}})()
+		matches = matches.concat(tok_matches);
 	}
-}
-function findBestMatch(rawSet) {
-	var longest = {word: '', length: 0};
-	for (var word in vocabulary) {
-		if (word != "TRASH") {
-			var regstr = getRegexpStr(vocabulary[word]);
-			var reg = new RegExp("^"+regstr+"("+getRegexpStr(vocabulary["TRASH"])+")");
-			var matches = rawSet.match(reg);
-			if (matches && matches[0].length > longest.length) {
-				longest = {word: word, length: matches[0].length
-					, match: trashTrim(matches[0])};
-			}
-		}
+	var longest = {match:''};
+	for (var i in matches) {
+		if (matches[i] != null 
+			&& matches[i].match != null 
+			&& matches[i].match.length > longest.match.length
+		) longest = matches[i];
 	}
 	return longest;
 }
-processFile(firstFile);
-
-console.log(tokenStack)
-// tokenStack = tokenStack.reverse();
-var newStack = [];
-function processStackOnce() {
-	var seq = [];
-	var passes = 0;
-
-}
-
-function reduceTokenSeq(seq) {
-	var match = '';
-	for (var i in grammar) {
-		for (var matchSet in grammar[i].match) {
-			var tokenSeqSet = grammar[i].match[matchSet];
-			var matches = true;
-			if (tokenSeqSet.length == seq.length) {
-				for (var j = 0; j < tokenSeqSet.length; j++) {
-					if (tokenSeqSet[j] != seq[j].word) matches = false;
-				}
-				if (matches) {
-					match = {word: i, val: seq};
-					return match;
-				}
-			}
-		}
-	}
-}
-processStackOnce();
-
-
-
-
