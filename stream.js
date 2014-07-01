73eba0beaab008d6686c78bb5c1033f2734ca1da
Sammons <bsammnz@gmail.com>
2014-07-01 17:29:59 -0500

    working on nonterminals now

--- stream.js
+++ stream.js
@@ -4,26 +4,18 @@ var args = process.argv.slice(2);
 if (args.length == 0) return console.log("not enough args");
 
 tokens = {
-	IF: /^if/,
-	LCURL: /^\{/,
-	RCURL: /^\}/,
-	STRING: /^\".*?\"|^\'.*?\'/,
-	MULTILINE_COMMENT: /^\/\*(.|\s)*?\*\//gim,
-	COMMENT: /^\/\/.*?$/,
-	NUMBER: /^[0-9\.]+/,
-	BITSHIFT: /^\>\>|^\<\</,
-	STMT_END: /^\n|^\;/,
-	IGNORE: /^\s/
+	GREETING: /^hello|^salutations|^sup|^what's happening|^what's new|^what's up|^hey|howdy/i,
+	ME: /^sammonsbot|^sb|^bot|^ben/i,
+	IGNORE: /^\s/ //necessary
 }
 
 non_terminals = {
-
+	PERSON_WELCOMING: { seq: [ 'GREETING', 'ME' ] }
 }
 
 var plaintext = fs.readFileSync(args[0])+'';
 var backup_plaintext = plaintext;
 var tokens_matched = []
-var stack = [];
 
 do {
 	var next = next_token(plaintext);
@@ -39,7 +31,35 @@ if (plaintext != '') {
 
 console.log(tokens_matched)
 
-function next_token(text, ignore) {
+reduce_stack_once(tokens_matched);
+reduce_stack_once(tokens_matched);
+
+console.log(tokens_matched)
+function reduce_stack_once(stack) {
+	var matches = [];
+	/* for every non-terminal, check the stack */
+	for (var i in non_terminals) {
+		var match_len = 0; /* aka, offset */
+		var pos = 0;
+		var seq = non_terminals[i].seq;
+		var match = {tok: '', seq: []};
+		while (pos < stack.length) {
+			/* check the stack against this non-terminal */
+		    if (stack[pos].tok == seq[match_len]) {
+		    	match_len++;
+		    	if (match_len == seq.length) {
+		    		console.log(non_terminals[i], i);
+		    		var matching_sequence = stack.splice(pos, match_len-1);
+		    		stack[pos-1] = (function(){ return {tok:i, seq: matching_sequence} } )();
+		    	}
+		    }
+		    pos++;
+		}
+	}
+}
+
+
+function next_token(text) {
 	var matches = [];
 	for (var i in tokens) {
 		var tok_matches = text.match(tokens[i]);
